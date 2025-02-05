224dd3aaa1855057345fad88a998a5bfb8285c91
AbhimanyuKoushik <abhimanyukoushikg@gamil.com>
2025-02-06 02:17:42 +0530

    commit

---
+++ bashscripts/gitpush.sh
@@ -0,0 +1,32 @@
+# Just the command gitpush <commit message> pushes all the changes with the given commit message, if the commit message is empty, it is set to the default message 'commit'
+
+#Make sure to run the below commands in the repo before using it
+#-> git remote add origin https://github.com/<username>/<reponame>.git
+#-> git remote set-url origin https://[token]@github.com/<username>/<reponame>
+# They change git push such that you dont need to give username or password each time you give the command git push
+
+#!/bin/bash
+
+# Default commit message if none is provided
+commit_message=${1:-"commit"}
+
+# Add all changes
+git add .
+
+# Commit with the provided or default message
+git commit -m "$commit_message"
+
+# Push changes
+git push
+
+# Gives the below message once the changes are successfully pushed
+echo "Changes pushed successfully!"
+
+#To run this bash script you have two options --
+#1. Either copy this bash script in every repo, make it executable using the command chmod +x gitpush.sh and then use it or
+#2. Keep it in a place where it is accessible to every directory. Usually such a place can be found by running the command echo $PATH. For the current device, it turns out to be /use/local/bin. Run the following commands to move it and make it an executable file
+# -->sudo mv gitpush.sh /usr/local/bin/gitpush
+# -->sudo chmod +x /usr/local/bin/gitpush
+# Now the file should be executable from anywhere
+# To delete the following bash script run sudo rm /usr/local/bin/gitpush
+
